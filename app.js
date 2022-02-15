8b60c4c51e0753eb6ab57997767c1d227896fa34
sevasvaldez <marcelosebastianvaldez@gmail.com>
2022-02-15 02:02:05 -0300

    Nuevo modal creado a mano, falta corregir para que las imagenes no se repitan

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const pintarCard = (data) => {
   const cards = document.getElementById("card-dinamicas");
   const templateCard = document.getElementById("template-card").content;
   const fragment = document.createDocumentFragment();
+  const modalDinamico = document.getElementById("modal-dinamico");
+  
   data.results.forEach((item) => {
     const clone = templateCard.cloneNode(true);
     clone.querySelector("h5").textContent = item.name;
@@ -29,11 +31,13 @@ const pintarCard = (data) => {
     clone.getElementById("p2").textContent = item.origin.name;
 
     clone.querySelector(".card-img-top").setAttribute("src", item.image);
-    clone.getElementById("card").addEventListener("click", () => {
-      console.log(item.name);
+    clone.getElementById("card").addEventListener("click", (e) => {
+      e.preventDefault();
+      modalDinamico.classList.add("modal--show");
+
       pintaModal(item);
-      
     });
+    
 
     //guardamos en el fragment para evitar el reflow
     fragment.appendChild(clone);
@@ -43,20 +47,31 @@ const pintarCard = (data) => {
 };
 
 const pintaModal = (item) => {
+  
   const modalDinamico = document.getElementById("modal-dinamico");
   const templateModal = document.getElementById("template-Modal").content;
   const fragment = document.createDocumentFragment();
-  console.log(templateModal)
+  console.log(templateModal);
   const clone = templateModal.cloneNode(true);
-  
-  clone.getElementById("nameChar").textContent = item.name;
-  clone.getElementById("aditionalInfo").textContent = item.status;
 
-  
+  modalDinamico.classList.add("modal--show");
+  clone.getElementById("nameChar").textContent = item.name;
+  clone.getElementById("imgChar").setAttribute("src", item.image);
+  clone.getElementById("cerrarModal").addEventListener("click", (e)=>{
+    e.preventDefault();
+      modalDinamico.classList.remove("modal--show");
+
+  })
+
   fragment.appendChild(clone);
   modalDinamico.appendChild(fragment);
-  
 };
+
+// const cerrarModal = () => {
+//   const modalDinamico = document.getElementById("modal-dinamico");
+
+//   modalDinamico.classList.remove("modal--show");
+// };
 
 const loadingData = (estado) => {
   const loading = document.getElementById("loading");
